1d9727f570bf08bb05ccc4115b35df9f93249b88
Dilshara Hasanka <86412386+dilshara26@users.noreply.github.com>
2023-04-04 23:42:38 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from src.dataSchema import Application
+from src.dataSchema import Job
+from src.main import ModelMain
+
+from src.databaseCont import (
+    createApplication,
+    getAllApplications,
+    getJobs,
+    createJob
+
+)
+
+app = FastAPI()
+
+origins = ['http://127.0.0.1:5173','http://127.0.0.1:5174']
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+# post controller
+
+
+
+@app.post("/api/application")
+async def post_application(application: Application):
+    Model = ModelMain()
+    print(application)
+    application.prediction = Model.runnerClass(application.answers[0])
+    response = await createApplication(application.dict())
+    if response:
+        return application.prediction
+
+@app.get("/api/admin")
+async def get_applications():
+    response = await getAllApplications()
+    return response
+
+
+@app.get("/api/jobs")
+async def get_jobs():
+    respone = await getJobs()
+    return respone
+# post jobs
+
+@app.post("/api/jobs")
+async def post_jobs(jobs:Job):
+    response = await createJob(jobs.dict())
+    if response:
+        return response
+
+
+
+
