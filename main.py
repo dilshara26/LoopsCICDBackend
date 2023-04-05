8c5c18a53b05c57ce30265339db633b5bea8785a
dilshara26 <dilsharahasanka@gmail.com>
2023-04-05 20:20:51 +0530

    origins secured

--- main.py
+++ main.py
@@ -23,12 +23,12 @@ nltk.download('wordnet')
 
 app = FastAPI()
 
-origins = ['http://127.0.0.1:5173','http://127.0.0.1:5174']
+origins = ['http://127.0.0.1:5173','http://127.0.0.1:5174','https://eclectic-cocada-9a268d.netlify.app/','https://eclectic-cocada-9a268d.netlify.app/']
 
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
